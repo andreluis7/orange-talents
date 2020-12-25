9806da239000463b8f3df2dd4e53715db11c189e
Andre Luis <adrluis7@gmail.com>
2020-12-25 13:18:58 -0300

    Project created

---
+++ backend/src/main/java/io/github/andreluis7/orangetalents/OrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package io.github.andreluis7.orangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalentsApplication.class, args);
+	}
+
+}
