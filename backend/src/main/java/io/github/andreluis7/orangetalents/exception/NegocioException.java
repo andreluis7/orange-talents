21e500659a098367d6b46fc872f0fd87a5a0f241
Andre Luis <adrluis7@gmail.com>
2020-12-26 23:02:54 -0300

    Tratamento de excessoes

---
+++ backend/src/main/java/io/github/andreluis7/orangetalents/exception/NegocioException.java
@@ -0,0 +1,10 @@
+package io.github.andreluis7.orangetalents.exception;
+
+public class NegocioException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public NegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ backend/src/main/java/io/github/andreluis7/orangetalents/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,10 @@
+package io.github.andreluis7.orangetalents.exception;
+
+public class RecursoNaoEncontradoException extends NegocioException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RecursoNaoEncontradoException (String message) {
+		super(message);
+	}
+}
