218ead1ed8a8f98103b752c670fd705e6865f4da
Andre Luis <adrluis7@gmail.com>
2020-12-28 11:58:04 -0300

    Implementando os validadores personalizados no DTO de cliente

--- backend/src/main/java/io/github/andreluis7/orangetalents/dto/request/ClienteDTO.java
+++ backend/src/main/java/io/github/andreluis7/orangetalents/dto/request/ClienteDTO.java
@@ -13,7 +13,8 @@ import org.hibernate.validator.constraints.br.CPF;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.github.andreluis7.orangetalents.model.Cliente;
-import io.github.andreluis7.orangetalents.validation.annotations.ValorUnico;
+import io.github.andreluis7.orangetalents.validation.annotations.CpfUnico;
+import io.github.andreluis7.orangetalents.validation.annotations.EmailUnico;
 
 public class ClienteDTO implements Serializable {
 
@@ -24,12 +25,12 @@ public class ClienteDTO implements Serializable {
 
 	@NotBlank(message = "O CPF do Cliente é obrigatório.")
 	@CPF(message = "Por favor digite um CPF válido.")
-	@ValorUnico(domainClass = Cliente.class, fieldName = "cpf", message = "Já existe um cadastro de cliente com esse CPF.")
+    @CpfUnico(message = "Já existe um cadastro de cliente com esse CPF")
 	private String cpf;
 
 	@NotBlank(message = "O Email do Cliente é obrigatório.")
 	@Email(message = "Por favor digite um Email válido.")
-	@ValorUnico(domainClass = Cliente.class, fieldName = "email", message = "Já existe um cadastro de cliente com esse Email.")
+    @EmailUnico(message = "Já existe um cadastro de cliente com esse Email")
 	private String email;
 
 	@NotNull(message = "Por favor digite uma data de nascimento.")
--- backend/src/main/java/io/github/andreluis7/orangetalents/repository/ClienteRepository.java
+++ backend/src/main/java/io/github/andreluis7/orangetalents/repository/ClienteRepository.java
@@ -1,8 +1,16 @@
 package io.github.andreluis7.orangetalents.repository;
 
-import io.github.andreluis7.orangetalents.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.andreluis7.orangetalents.model.Cliente;
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	Optional<Cliente> findByCpf(String cpf);
+	Optional<Cliente> findByEmail(String email);
 
 }
